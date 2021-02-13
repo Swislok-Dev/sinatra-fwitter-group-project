0ecac46d6f8eeba9965d993093047f52b3a15f56
Cory <cmarker4@gmail.com>
2021-02-13 17:06:24 -0600

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
 
+  get '/users/:slugs' do
+    @user = User.find_by_slug(params[:slug])
+    @tweets = @user.tweets
+
+    erb :'users/show'
+  end
 
 end
